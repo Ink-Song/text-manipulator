beb943fe0cd405dfa36aa13cf3b9c42b65b20756
Ink-Song <sunruh@proton.me>
2025-02-10 14:07:41 +0100

    Project Setup

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $io.github.ink-song.idatt2003;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
