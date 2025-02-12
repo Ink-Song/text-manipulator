6e8c11496aed7e9e550769847d620fcb573ef6c0
Ink-Song <sunruh@proton.me>
2025-02-12 09:22:46 +0100

    Feature: Added test class for CapitalizeSelectionTextCommand

---
+++ src/test/java/io/github/ink_song/idatt2003/text_manipulator/CapitalizeSelectionTextCommandTest.java
@@ -0,0 +1,20 @@
+package io.github.ink_song.idatt2003.text_manipulator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CapitalizeSelectionTextCommandTest {
+
+  CapitalizeSelectionTextCommand command = new CapitalizeSelectionTextCommand("selection");
+  @Test
+  void execute() {
+    String result = command.execute("Here's a selection and a second selection.");
+    assertEquals("Here's a Selection and a second Selection.", result);
+    System.out.println(result);
+  }
+
+  @Test
+  void getSelection() {
+  }
+}
