00dc8d31fd40d43e7e50720f1298e3fa1004af9e
Ink-Song <sunruh@proton.me>
2025-02-12 08:51:24 +0100

    Feature: Created WrapSelectionTextCommand

---
+++ src/main/java/io/github/ink_song/idatt2003/text_manipulator/WrapSelectionTextCommand.java
@@ -0,0 +1,14 @@
+package io.github.ink_song.idatt2003.text_manipulator;
+
+public class WrapSelectionTextCommand extends WrapTextCommand{
+  private String selection;
+  public WrapSelectionTextCommand(String open, String close, String Selection) {
+    super(open, close);
+    this.selection = Selection;
+  }
+
+  @Override
+  public String execute(String text) {
+    return text.replace(selection, super.execute(selection));
+  }
+}
