bc738b465d30fb65e5295470f56a1316a85552a0
Ink-Song <sunruh@proton.me>
2025-02-12 08:54:57 +0100

    Test: Created WrapSelectionTextCommand test

---
+++ src/test/java/io/github/ink_song/idatt2003/text_manipulator/WrapSelectionTextCommandTest.java
@@ -0,0 +1,16 @@
+package io.github.ink_song.idatt2003.text_manipulator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WrapSelectionTextCommandTest {
+
+  WrapSelectionTextCommand command = new WrapSelectionTextCommand("<p>","</p>","Selection");
+  @Test
+  void execute() {
+    String result = command.execute("This shouldn't be enclosed. This (Selection) should be.");
+    assertEquals("This shouldn't be enclosed. This (<p>Selection</p>) should be.", result);
+    System.out.println(result);
+  }
+}
